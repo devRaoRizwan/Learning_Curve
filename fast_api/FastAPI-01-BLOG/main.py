d3b420587cbc96b278cb9e684f80bcd08e6d144a
devRaoRizwan <dev.raorizwan@gmail.com>
2026-09-14 07:20:28 +0500

    feat:added a html page using jinja2

--- fast_api/FastAPI-01-BLOG/main.py
+++ fast_api/FastAPI-01-BLOG/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 posts: list[dict] = [
@@ -21,10 +23,10 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/" , response_class= HTMLResponse , include_in_schema=False) # we will add include_in_schema key to ensure it doesnt shows in the docs ( the html endpoints)
-@app.get("/posts" , response_class=HTMLResponse) #what we did here is mapped two diff endpoints(/ and /posts) to same function
-def home() :
-    return f"<h1>{posts[0]["title"]}</h1>"
+@app.get("/" , include_in_schema=False) # we will add include_in_schema key to ensure it doesnt shows in the docs ( the html endpoints)
+@app.get("/posts", include_in_schema=False) #what we did here is mapped two diff endpoints(/ and /posts) to same function
+def home(request : Request) :
+    return templates.TemplateResponse(request, name="home.html")
 
 @app.get("/api/posts")
 def get_posts():

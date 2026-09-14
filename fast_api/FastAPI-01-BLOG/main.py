3688afe0d88e19cfe6e3bcdd1af9baa69c44bc29
devRaoRizwan <dev.raorizwan@gmail.com>
2026-09-14 12:28:20 +0500

    added the create post endpoint with schema validation

--- fast_api/FastAPI-01-BLOG/main.py
+++ fast_api/FastAPI-01-BLOG/main.py
@@ -39,3 +39,17 @@ def get_post(post_id : int):
         if post.get("id") == post_id :
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail="post not found")
+
+
+@app.post("/api/posts" , response_model= PostCreate  , status_code= status.HTTP_201_CREATED)
+def create_post(post : PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id" : new_id,
+        "author" : post.author,
+        "title" : post.title,
+        "content" : post.content,
+        "date_posted" : "September 14, 2026",
+    }
+    posts.append(new_post)
+    return new_post
